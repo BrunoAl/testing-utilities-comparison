9ad3e2f82852d0ab2e9cc0fbc05f051aeb138e83
Bruno <brunoalnascimento@gmail.com>
2018-12-15 15:20:05 -0200

    complete Input component render tests for success `false`

--- jotto/src/enzymeTestUtils.js
+++ jotto/src/enzymeTestUtils.js
@@ -1,4 +1,16 @@
 import checkPropTypes from 'check-prop-types'
+import { createStore } from 'redux'
+
+import rootReducer from './reducers'
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state.
+ * globals: rootReducer.
+ * @param {object} initialState - Initial state for store.
+ * @returns {Store} - Redux store.
+ */
+export const storeFactory = initialState => createStore(rootReducer, initialState)
+
 /**
  * Return node(s) with the given data-testid attribute.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
