7efa569152b0ef9d120cc6d03ff4f1364db1e3c8
Bruno <brunoalnascimento@gmail.com>
2018-12-15 15:30:30 -0200

    complete Input component render tests for success `true`

--- jotto/src/__tests__/enzyme/Input.test.js
+++ jotto/src/__tests__/enzyme/Input.test.js
@@ -40,9 +40,23 @@ describe('render', () => {
   })
 
   describe('word has been guessed', () => {
-    test('renders component without error', () => {})
-    test('does not renders input box', () => {})
-    test('does not renders submit button', () => {})
+    let wrapper
+    beforeEach(() => {
+      const initialState = { success: true }
+      wrapper = setup(initialState)
+    })
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input')
+      expect(component.length).toBe(1)
+    })
+    test('does not renders input box', () => {
+      const component = findByTestAttr(wrapper, 'input-box')
+      expect(component.length).toBe(0)
+    })
+    test('does not renders submit button', () => {
+      const component = findByTestAttr(wrapper, 'submit-button')
+      expect(component.length).toBe(0)
+    })
   })
 })
 
