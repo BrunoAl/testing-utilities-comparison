710d44e3f5d670371c34563738eca0157cd1ab0e
Bruno <brunoalnascimento@gmail.com>
2018-12-15 10:41:08 -0200

    tests and code for non-zero guessed words

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -5,7 +5,27 @@ function GuessedWords({ guessedWords }) {
   const contents =
     guessedWords.length === 0 ? (
       <span data-testid="guess-instructions">Try to guess the secret word!</span>
-    ) : null
+    ) : (
+      <div data-testid="guessed-words">
+        <h3>GuessedWords</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>
+            {guessedWords.map((word, index) => (
+              <tr data-testid="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
   return <div data-testid="component-guessed-words">{contents}</div>
 }
 
